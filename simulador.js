83ed0bc8e507dd421ef3c78868303e1e1314ba29
kevin noriega <kevin.noriega96@gmail.com>
2022-06-28 20:04:24 -0300

    simulador

--- simulador.js
+++ simulador.js
@@ -6,9 +6,6 @@ function sumar(number1, number2) {
 function operacion(m,d) {
     return   m * tasa / d
 }
-function mostrar(mensaje) {
-    console.log(mensaje);
-}
 
 
 
@@ -19,17 +16,18 @@ function plazofijo(m, d, cambio) {
     let resultado = 0;
 
     switch (cambio) {
+
         case "p":
             
-           rdo=operacion(m,d).toFixed(2);  
+           rdo=operacion(m,d);  
            resultado = sumar(m, rdo);
-            console.log( 'El interes a percibir es de  $' +rdo+' al cabo de '+ dias+' dias su capital sera $')
+            console.log( 'El interes a percibir es de  $' +rdo.toFixed(2)+' al cabo de '+ dias+' dias su capital final sera $'+resultado.toFixed(2))
             break;
         case "d":
            
-           rdo=operacion(m,d).toFixed(2);
+           rdo=operacion(m,d);
             resultado = sumar(m, rdo);
-            console.log( 'El interes a percibir es de  $' +rdo+' al cabo de '+ dias+' dias su capital sera $'+resultado)
+            console.log( 'El interes a percibir es de  U$D ' +rdo.toFixed(2)+' al cabo de '+ dias+' dias su capital final sera  U$D '+resultado.toFixed(2))
             break;
        
         default:
@@ -40,6 +38,7 @@ return resultado;
 let monto = parseInt(prompt('Ingrese el monto a invertir '));
 let dias = parseInt(prompt('Ingrese la cantidad de dias '));
 let tasa=parseFloat(prompt('ingrese la tasa ofrecida '))
-let tipo = prompt('Ingresa la moneda,  P para peso o D para dolar')
-mostrar(plazofijo(monto,dias,tipo));
- 
+let tipo = prompt('Ingresa la moneda,  P para peso o D para dolar').toLowerCase()
+
+plazofijo(monto,dias,tipo);
+
