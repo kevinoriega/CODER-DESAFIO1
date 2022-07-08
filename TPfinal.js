68def4886b72a436ca584a704b3193a648c6cac8
kevin noriega <kevin.noriega96@gmail.com>
2022-07-07 23:28:45 -0300

    primera entrega

---
+++ TPfinal.js
@@ -0,0 +1,50 @@
+ let numero=0
+function sumar(number1, number2) {
+    let suma = number1 + number2
+    return suma;
+}
+
+
+let producto= parseInt(prompt('Ingrese precio del producto'))
+const precios=[ producto];
+
+
+
+ function  monto( monto )  {
+    this.monto  =  monto ;
+    
+  }
+
+  
+  
+  function  sumatotal( precios)  {
+   
+    precios.push( precios) ;
+  }
+  
+ 
+
+ function agregar( n){
+       
+      
+       if (condicion== "si"){
+        condicion== true
+        sumatotal(n)
+         monto=sumar( n,total);
+         
+       }else ('gracias')
+    }
+
+
+total = sumar(numero,producto)
+ alert('total a pagar  $ ' + total+' agregar otro producto? ') 
+ let condicion = prompt(' si o no').toLowerCase()
+
+ let n=parseInt(prompt('Ingrese precio del producto'))
+ agregar(n)
+
+ console.log(precios);
+ for (  i=0;i< precios.length; i++){
+    console.log(precios[i])
+ }
+ alert('total a pagar  $' + monto)
